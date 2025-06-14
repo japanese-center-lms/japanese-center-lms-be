12eb5a53ecc7c162cbb2af50d724e3b2366e9dd4
namdang-fdp <namdangcoder@gmail.com>
2025-06-14 20:23:13 +0700

    feat: use UUID instead of integer

--- src/main/java/com/hinata/lms/entity/User.java
+++ src/main/java/com/hinata/lms/entity/User.java
@@ -6,12 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @Builder
@@ -19,11 +21,10 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "user")
-
 public class User implements UserDetails {
     @Id
     @GeneratedValue
-    private Integer id;
+    private UUID id;
     private String username;
     private String password;
     private String email;
