078b3a0f18c8ea7e74442f3fa50ffa31a0b0d081
namdang-fdp <namdangcoder@gmail.com>
2025-06-14 18:59:37 +0700

    feat: add token repo interface

---
+++ src/main/java/com/hinata/lms/repositories/TokenRepository.java
@@ -0,0 +1,10 @@
+package com.hinata.lms.repositories;
+
+import java.util.Optional;
+
+import com.hinata.lms.entity.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+    Optional<Token> findByToken(String token);
+}
