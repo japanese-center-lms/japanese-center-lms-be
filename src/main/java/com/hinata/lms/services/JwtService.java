a267bc9e23d40bb17a792f31f91eee22b448105d
namdang-fdp <namdangcoder@gmail.com>
2025-06-14 23:06:10 +0700

    fix(refresh): typo error

--- src/main/java/com/hinata/lms/services/JwtService.java
+++ src/main/java/com/hinata/lms/services/JwtService.java
@@ -21,7 +21,7 @@ public class JwtService {
     private String secretKey;
     @Value("${application.security.jwt.expiration}")
     private long jwtExpiration;
-    @Value("${application.security.jwt.refrest-token.expiration}")
+    @Value("${application.security.jwt.refresh-token.expiration}")
     private long refreshExpiration;
 
     private Key getSignInKey() {
