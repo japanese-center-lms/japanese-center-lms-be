f44ea9ba98e8341566e1a9e06c199b89de10f820
namdang-fdp <namdangcoder@gmail.com>
2025-06-14 15:05:42 +0700

    feat: add swagger definition

--- src/main/java/com/hinata/lms/configs/OpenApiConfig.java
+++ src/main/java/com/hinata/lms/configs/OpenApiConfig.java
@@ -1,15 +1,48 @@
 package com.hinata.lms.configs;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name="namdang-fdp",
+                        email= "namdangcoder@gmail.com",
+                        url="https://github.com/namdang-fdp"
+                ),
+                description = "API for HinataLMS",
+                title = "HinataLMS",
+                version = "1.0",
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "https://localhost:8080"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
 @SecurityScheme(
         name = "Bearer Authentication",
+        description = "JWT auth description",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
-        scheme = "bearer"
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
 }
