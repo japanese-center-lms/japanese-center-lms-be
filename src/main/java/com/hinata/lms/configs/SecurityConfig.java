ec5189760d955a17183081f62abe60e23a01375f
namdang-fdp <namdangcoder@gmail.com>
2025-06-14 22:13:47 +0700

    feat: advance config in security config

--- src/main/java/com/hinata/lms/configs/SecurityConfig.java
+++ src/main/java/com/hinata/lms/configs/SecurityConfig.java
@@ -8,8 +8,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static com.hinata.lms.enums.Permission.ADMIN_CREATE;
+import static com.hinata.lms.enums.Permission.ADMIN_READ;
+import static com.hinata.lms.enums.Permission.ADMIN_UPDATE;
+import static com.hinata.lms.enums.Permission.ADMIN_DELETE;
+import static com.hinata.lms.enums.Permission.TEACHER_CREATE;
+import static com.hinata.lms.enums.Permission.TEACHER_READ;
+import static com.hinata.lms.enums.Permission.TEACHER_UPDATE;
+import static com.hinata.lms.enums.Permission.TEACHER_DELETE;
+import static com.hinata.lms.enums.Role.ADMIN;
+import static com.hinata.lms.enums.Role.TEACHER;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.PUT;
 
 @Configuration
 @EnableWebSecurity
@@ -25,11 +42,29 @@ public class SecurityConfig {
     private final LogoutHandler logoutHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
-                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), TEACHER.name())
+                                .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), TEACHER_READ.name())
+                                .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), TEACHER_READ.name())
+                                .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), TEACHER_READ.name())
+                                .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), TEACHER_READ.name())
+                                .anyRequest()
+                                .authenticated()
+                )
+                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                 .authenticationProvider(authenticationProvider)
+                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                 .logout(logout ->
+                         logout.logoutUrl("/api/v1/auth/logout")
+                                 .addLogoutHandler(logoutHandler)
+                                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+
+                 );
+         return httpSecurity.build(); 
     }
 }
 
