5def31601f17116772f5982d583e2f64c99b85ad
namdang-fdp <namdangcoder@gmail.com>
2025-06-14 20:25:43 +0700

    feat: change the generic to Token, UUID

--- src/main/java/com/hinata/lms/repositories/TokenRepository.java
+++ src/main/java/com/hinata/lms/repositories/TokenRepository.java
@@ -1,10 +1,11 @@
 package com.hinata.lms.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import com.hinata.lms.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TokenRepository extends JpaRepository<Token, Integer> {
+public interface TokenRepository extends JpaRepository<Token, UUID> {
     Optional<Token> findByToken(String token);
 }
