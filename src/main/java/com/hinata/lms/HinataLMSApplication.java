288e2c891049c2612ab25e1974f88bca35b69a05
namdang-fdp <namdangcoder@gmail.com>
2025-06-15 20:31:41 +0700

    feat: add bcrypt println for testing

--- src/main/java/com/hinata/lms/HinataLMSApplication.java
+++ src/main/java/com/hinata/lms/HinataLMSApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HinataLMSApplication {
 
 	public static void main(String[] args) {
+		System.out.println(new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().encode("123456"));
 		SpringApplication.run(HinataLMSApplication.class, args);
 	}
 
